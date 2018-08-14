237daaa8f68e8deea73f572ba4845801309a488d
nebevets <steveben@exmsft.com>
2018-08-14 12:47:01 -0700

    changed parameter name

--- practice29/practice29.js
+++ practice29/practice29.js
@@ -1,21 +1,21 @@
 // is there a better way than switch?
 
-function do_math(number, num2, string){
+function do_math(num1, num2, string){
     var operators = ['+', '-', '/', '*', 'x', 'X'];
     var operatorIndex = operators.indexOf(string);
     switch (operatorIndex){
         case 0:
-            return parseFloat(number) + parseFloat(num2);
+            return parseFloat(num1) + parseFloat(num2);
         case 1:
-            return parseFloat(number) - parseFloat(num2);
+            return parseFloat(num1) - parseFloat(num2);
         case 2:
-            return parseFloat(number) / parseFloat(num2);
+            return parseFloat(num1) / parseFloat(num2);
         case 3:
         case 4:
         case 5:
-            return parseFloat(number) * parseFloat(num2);
+            return parseFloat(num1) * parseFloat(num2);
         default:
-            return 'error, unknown operator';
+            return 0;
     }
 }
 
