b2bd770470675b2abc26ec669d01b27dca081f81
nebevets <steveben@exmsft.com>
2018-08-14 17:40:40 -0700

    practice30 - steve benedict

---
+++ practice30/practice30.js
@@ -0,0 +1,13 @@
+function random_range(start_num, end_num){
+    var min, max;
+    if (Array.isArray(start_num) && end_num === undefined){
+        min = 0, max = start_num.length-1;
+        return start_num[Math.floor(Math.random()* (max-min+1))+min];
+    }else{
+        min = start_num, max = end_num;
+    }
+    return Math.floor(Math.random()* (max-min+1))+min;
+}
+console.log(random_range(2,15));
+console.log(random_range(1,100));
+console.log(random_range(['a','b','c','d','e']));
