aada3542717ed1c47967f05f5cdfa5b5c3f9629d
nebevets <steveben@exmsft.com>
2018-10-28 08:19:14 -0700

    changed while condition

--- practice06/practice06.js
+++ practice06/practice06.js
@@ -4,9 +4,9 @@ function fibSequence(number){
         console.log("please provide a number.");
         return;
     }
-    let results = [];
+    const results = [];
     let newFibNum = 0;
-    while (!(results.length === number)){
+    while (results.length !== number){
         newFibNum = results[results.length-2] + results[results.length-1] || results.length;
         results.push(newFibNum);
     }
