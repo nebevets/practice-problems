2b8468d26565e409a9e47cbb1cd73e7af28c84e5
nebevets <steveben@exmsft.com>
2018-11-05 09:46:50 -0800

    practice07 - steve benedict

--- practice07/practice07.js
+++ practice07/practice07.js
@@ -1,17 +1,25 @@
-function akemayStrIgpayAtinLay(string){
-    let esultray = '';
-    let ay = 'ay';
-    const lookUpValue = {
+function translate(string){
+    let result = [];
+    let suffix = 'ay';
+    const lookUpVowel = {
         a: 'a', e: 'e', i: 'i', o: 'o', u: 'u'
     }
-    let original = string;
+    //let original = string;
     string = string.toLowerCase();
+    const words = string.split(' ');
     
-    for(let i = 0; i < string.length; i++){
-       if (lookUpValue[string[i]]){
-           esultray = original.substr(i) + original.substr(0, i) + ay;
-           break;
-       }
+    for(word in words){
+        let newWord;
+        for(letter in words[word]){
+            if(lookUpVowel[words[word][letter]]){
+                newWord = words[word].substr(letter) + words[word].substr(0,letter) + suffix;
+                result.push(newWord);
+                break;
+            }
+        }
+        if (!newWord){
+            result.push(words[word]);
+        }
     }
-    return esultray;
+    return result.join(' ');
 }
