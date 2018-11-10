dbf29c567fbed5860cafab9b4662a9faf08e6e47
nebevets <steveben@exmsft.com>
2018-11-10 11:57:20 -0800

    practice13 - steve benedict

--- practice13/practice13.js
+++ practice13/practice13.js
@@ -1,10 +1,10 @@
 const doMathCombos = (num1, num2)=>{
     const results = [];
     const ops = {
-        '+': ()=>{return num1 + num2},
-        '-': ()=>{return num1 - num2},
-        '/': ()=>{return num1 / num2}, 
-        '*': ()=>{return num1 * num2}  
+        '+': ()=>num1 + num2,
+        '-': ()=>num1 - num2,
+        '/': ()=>num1 / num2, 
+        '*': ()=>num1 * num2  
     }
     for (var key in ops){
         results.push(`${num1}${key}${num2}=${ops[key]()}`);
