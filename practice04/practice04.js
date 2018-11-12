0f032ea61d7bffbd0c3bab86bd87fc3e1ab8eae4
nebevets <steveben@exmsft.com>
2018-11-12 11:48:14 -0800

    practice04 - steve benedict

--- practice04/practice04.js
+++ practice04/practice04.js
@@ -1,35 +1,35 @@
-function sortStringsArray(strings){
-    var sorted = false;
-    while (sorted === false){
+const bubbleSort = (array)=>{
+    let sorted;
+    do {
         sorted = true;
-        var i = 0;
-        while(strings[i+1] !== undefined){
-            if(strings[i] > strings[i+1]){
-                var temp = strings[i];
-                strings[i] = strings[i+1];
-                strings[i+1] = temp;
+        let i = 0;
+        while(array[i+1] !== undefined){
+            if(array[i] > array[i+1]){
+                let temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
                 sorted = false;
             }
             i++;
         }
-    }
-    return strings;
+    }while (!sorted);
+    return array;
 }
 
 var test1 = {
     input: ['e', 'd', 'a', 'b', 'c'],
     expect: ['a', 'b', 'c', 'd', 'e'],
-    actual: sortStringsArray(['e', 'd', 'a', 'b', 'c'])
+    actual: bubbleSort(['e', 'd', 'a', 'b', 'c'])
 }
 var test2 = {
     input: ['mouse', 'cat', 'dog', 'human'],
     expect: ['cat', 'dog', 'human', 'mouse'],
-    actual: sortStringsArray(['mouse', 'cat', 'dog', 'human'])
+    actual: bubbleSort(['mouse', 'cat', 'dog', 'human'])
 }
 var test3 = {
     input: ['a', 'b'],
     expect: ['a', 'b'],
-    actual: sortStringsArray(['a', 'b'])
+    actual: bubbleSort(['a', 'b'])
 }
 
 console.log(test1, test2, test3);
