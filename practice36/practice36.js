5d9d816757d47da2cca75a009a3fc72af6956279
nebevets <steveben@exmsft.com>
2018-12-19 10:18:31 -0800

    practice36 - steve benedict

--- practice36/practice36.js
+++ practice36/practice36.js
@@ -12,3 +12,18 @@ function findVowels(string){
 }
 
 console.log(findVowels("four score and seven years ago"));
+
+const newFindVowels = string => {
+    const results = {};
+    const vowels = /[AEIOUaeiou]+/;
+    for(let letter = 0; letter < string.length; letter++){
+        if (vowels.test(string[letter])){
+            results[string[letter]] ? 
+                results[string[letter]]++:
+                results[string[letter]] = 1;
+        }
+    }
+    return results;
+}
+
+console.log(newFindVowels("four score and seven years ago"));
