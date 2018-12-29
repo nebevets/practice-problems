df09f5bf693f7a871c21eeaba1f6010715360cc4
nebevets <steveben@exmsft.com>
2018-12-29 13:54:13 -0800

    finished 1 and 2

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,10 +1,18 @@
 
-function sumArray(){
-	
+function sumArray(array){
+	return array.reduce((total,item)=>total+=item);
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, number){
+    const result = [];
+    let total = 0;
+    for(var index = 0; index < array.length; index++){
+        if (array[index]+total <= number){
+            result.push(array[index]);
+            total += array[index];
+        }
+    }
+    return result;
 }
 
 function getAllNamesShorterThan(){
