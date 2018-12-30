c458999bad554f7a0875aec5226129a3b6ffd150
nebevets <steveben@exmsft.com>
2018-12-29 19:44:31 -0800

    drill 1 - steve benedict

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -5,20 +5,22 @@ function sumArray(array){
 
 function fitWithinVal(array, number){
     const result = [];
-    let total = 0;
-    for(var index = 0; index < array.length; index++){
+    let total = 0, index =0;
+    do{
         if (array[index]+total <= number){
             result.push(array[index]);
             total += array[index];
         }
-    }
+        index++;
+    }while(index < array.length && total < number);
+
     return result;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(array, length){
+    return array.filter(item => item.length < length);
 }
 
-function makeLabel(){
-
+function makeLabel(person){
+    return `${person.greeting} ${person.givenName} ${person.familyName}\n${person["home address"].streetNumber} ${person["home address"].streetName}\n${person["home address"].city}, ${person["home address"].state} ${person["home address"].zip}`;
 }
