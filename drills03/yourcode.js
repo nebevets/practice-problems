330aecf95d4cea267e531cfd0a4f68bdfcecd2e7
nebevets <steveben@exmsft.com>
2019-01-02 12:54:44 -0800

    rough completion of url parser

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -5,9 +5,27 @@ function getPath(path){
     return folderNames;
 }
 
-function getPathParts(){
-    const regExURL = /^([FfIiLlEeHhTtPpSsMmAaOoDdRrCc]+):\/\/([A-Za-z\.]+)\:([\d]{1,5})\/([A-Za-z\/]+)\/([A-Za-z\.]+)$/
-
+function getPathParts(url){
+    const regExURL = /^([FfIiLlEeHhTtPpSsMmAaOoDdRrCc]+):\/\/([A-Za-z_\.\-]+)?:?(\d{1,5})?([A-Za-z_\.\/\-]+)?/;
+    if(!regExURL.test(url)){
+        return 'error: please enter a properly formatted URL, including the protocol.'
+    }
+    const results = {};
+    const parsedUrl = regExURL.exec(url);
+    const keys = ['protocol', 'host', 'port', 'path'];
+    keys.map((item,index)=>{
+        if(parsedUrl[index+1]){
+            results[item] = parsedUrl[index+1];
+        }
+    });
+    if(results.path && parsedUrl[4]){
+        results.path = results.path.substr(1, results.path.lastIndexOf('/') - 1);
+        results.file = parsedUrl[4].substr(parsedUrl[4].lastIndexOf('/') + 1);
+    }
+    if(results.port){
+        results.port = parseInt(results.port);
+    }
+    return results;
 }
 
 function getCapitalCount(){
